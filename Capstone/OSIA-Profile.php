2106cb89275e006192c1c229f009067b60f9d049
BSONGERISTS <135406685+BSONGERISTS@users.noreply.github.com>
2023-06-28 22:51:20 +0800

    Update OSIA-Profile.php

--- Capstone/OSIA-Profile.php
+++ Capstone/OSIA-Profile.php
@@ -28,7 +28,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST"){
         <div class="container">
             <div class="logcontainer">
                 <div class="circle">
-
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
+                    <path d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464H398.7c-8.9-63.3-63.3-112-129-112H178.3c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3z"/></svg>
                 </div>
 
                 <button class="scircle" id="ms" onclick="Upload()">
@@ -36,7 +37,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST"){
                     <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H322.8c-3.1-8.8-3.7-18.4-1.4-27.8l15-60.1c2.8-11.3 8.6-21.5 16.8-29.7l40.3-40.3c-32.1-31-75.7-50.1-123.9-50.1H178.3zm435.5-68.3c-15.6-15.6-40.9-15.6-56.6 0l-29.4 29.4 71 71 29.4-29.4c15.6-15.6 15.6-40.9 0-56.6l-14.4-14.4zM375.9 417c-4.1 4.1-7 9.2-8.4 14.9l-15 60.1c-1.4 5.5 .2 11.2 4.2 15.2s9.7 5.6 15.2 4.2l60.1-15c5.6-1.4 10.8-4.3 14.9-8.4L576.1 358.7l-71-71L375.9 417z"/></svg>
                 </button>
 
-                <div class="proftext"><b class="font"></b></div>
+                <div class="proftext"><b class="font">Profile Name</b></div>
                 <div class="leftname"><b class="font">Name</b></div>
                 <div class="leftusername"><b class="font">Username</b></div>
                 <div class="leftbirth"><b class="font">Date of Birth</b></div>
