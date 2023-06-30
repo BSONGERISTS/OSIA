adbe905fb18501d1243a35152b612e7eb8e3c695
Tinguisher <135411202+Tinguisher@users.noreply.github.com>
2023-07-01 02:49:57 +0800

    Update OSIA-Document.php

--- Capstone/OSIA-Document.php
+++ Capstone/OSIA-Document.php
@@ -3,7 +3,7 @@
 session_start();
 if(isset($_SESSION["id"])){
     $mysqli = require __DIR__ . "/database.php";
-    $sql = "SELECT * FROM users WHERE id = {$_SESSION["id"]}";
+    $sql = "SELECT * FROM users WHERE id = '". $_SESSION["id"] ."'";
     $result = $mysqli->query($sql);
     $username = $result->fetch_assoc();
 }
@@ -12,13 +12,42 @@ else{
     header("Location: OSIA-LogIn.php");
     exit;
 }
+
+
+function print_a( $string ){
+    
+    echo "<pre>";
+    print_r( $string );
+    echo "</pre>";
+    
+}
 ?>
 
 <html>
     <head>
         <link rel="stylesheet" href="OSIA-DocumentStyle.css">
         <link rel="icon" type="image/png" href="headerpng.png">
+        <script src="jquery-3.6.4.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+
+        <script>
+
+        $(document).ready(function ( $ ) {
+
+                $( "button.directories" ).on( "click", function(){
+                        var dirValue = $( this ).val();
+                        console.log( dirValue );
+                        $( "input.hidden_directory" ).val( dirValue );
+                        $( "label.hidden_directory" ).text( dirValue );
+                });
+        });
+
+  
+            
+
+        </script>
         <title>OSIA: Organized System Information Assistant</title>
+        
     </head>
     <body>
         
@@ -30,11 +59,13 @@ else{
                     <?php echo '<img  class="profileimg" src="data:image/jpeg;base64,' . base64_encode( $username["profile"] ) . '" />'; ?>
                 </button>
             </form>
-            <div class="elem1"></div>
+            <!-- <div class="elem1"></div> -->
             <button class="message" id="ms" onclick="Popout()"><b class="font">Message</b></button>
+
             <div class="preview">
-                <iframe src="uploads/13.pdf" frameborder="0" height="100%" width="100%"></iframe>
+                <iframe id="filepreview" src="" frameborder="0" height="100%" width="100%"></iframe>
             </div>
+            
             <div class="documentprev"><b class="font">Document Preview</b></div>
             <button class="delete">
                 <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
@@ -63,23 +94,70 @@ else{
                     <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
                 </button>
             </div>
-            
             <!--Files and Folders-->
             <div class="Folders">
                 <?php
                 //gets the file names
-                    $directory = "uploads/";
-                    if(is_dir($directory)){
-                        $files = opendir($directory);
-                        if($files){
-                            while(($file_name = readdir($files)) !== FALSE){
-                                if($file_name != '.' && $file_name != '..'){
-                                    echo '<button class="files">';
-                                    echo $file_name;
+                $sql = "SELECT * FROM osia";
+
+                $result = $mysqli->query( $sql );
+
+                $results = $result->fetch_all( MYSQLI_ASSOC );
+
+                if( !empty( $results ) ){
+
+                        $directories = array(
+                            "Capstone",
+                            "uploads"
+                        );
+                    
+                        foreach( $results as $key => $row ){
+
+                                $var_directory =  "";
+
+                                if( !empty( $row['directory'] ) ){
+
+                                    $var_directory = $row['directory'];
+                                    $filename = $row['file'];
                                 }
-                            }
+                                echo '<button type="text" class="files directories" value="'. $var_directory .'">' . $filename . '</button>';
                         }
-                    }
+                }
+
+                // if(is_dir($directory)){
+                //     $files = opendir($directory);
+                //     if($files){
+                //         $i=1;
+                //         while(($file_name = readdir($files)) !== FALSE){
+                            
+                            
+                            
+                //             if($file_name != '.' && $file_name != '..'){
+                                
+                //                 $btn = "button" . $i;
+                //                 echo '<button class="files" onclick="'. $btn . '()">';
+                //                     echo $file_name;
+                //                 echo '</button>';
+                                
+                //                 $folder = $directory;
+                //                 echo '<script type="text/javascript">';
+                //                     echo 'function ' . $btn . '(){';
+                //                     if (strpos($file_name, ".") !== false) {
+                //                         $directory = $directory ."/". $file_name;
+                //                         echo "document.getElementById('filepreview').setAttribute('src', '" . $directory . "');";
+                //                         $directory = $folder;
+                //                     }
+                //                     else{
+                                        
+                //                         //changed value of directory going to if(is_dir($directory))
+                //                     }
+                //                     echo '}';
+                //                 echo '</script>';
+                //                 $i++;
+                //             }
+                //         }
+                //     }
+                // }
 
                     /* dito muna
                     $filecount = count(glob($directory . "*"));
@@ -96,7 +174,11 @@ else{
                     
                 ?>
             </div>
-            <div class="foldertab"></div>
+
+            <div class="foldertab">
+                <label class="hidden_directory">uploads/</label>
+            </div>
+
             <button class="Previous">
                 <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
                 <path d="M177.5 414c-8.8 3.8-19 2-26-4.6l-144-136C2.7 268.9 0 262.6 0 256s2.7-12.9 7.5-17.4l144-136c7-6.6 17.2-8.4 26-4.6s14.5 12.5 14.5 22l0 72 288 0c17.7 0 32 14.3 32 32l0 64c0 17.7-14.3 32-32 32l-288 0 0 72c0 9.6-5.7 18.2-14.5 22z"/></svg>
@@ -105,18 +187,41 @@ else{
                 <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
                 <path d="M334.5 414c8.8 3.8 19 2 26-4.6l144-136c4.8-4.5 7.5-10.8 7.5-17.4s-2.7-12.9-7.5-17.4l-144-136c-7-6.6-17.2-8.4-26-4.6s-14.5 12.5-14.5 22l0 72L32 192c-17.7 0-32 14.3-32 32l0 64c0 17.7 14.3 32 32 32l288 0 0 72c0 9.6 5.7 18.2 14.5 22z"/></svg>
             </button>
-            <button class="add">
+
+
+            <!--if add folder is clicked-->
+            <button class="add" onclick="addfolder()">
                 <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
                 <path d="M512 416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96C0 60.7 28.7 32 64 32H192c20.1 0 39.1 9.5 51.2 25.6l19.2 25.6c6 8.1 15.5 12.8 25.6 12.8H448c35.3 0 64 28.7 64 64V416zM232 376c0 13.3 10.7 24 24 24s24-10.7 24-24V312h64c13.3 0 24-10.7 24-24s-10.7-24-24-24H280V200c0-13.3-10.7-24-24-24s-24 10.7-24 24v64H168c-13.3 0-24 10.7-24 24s10.7 24 24 24h64v64z"/></svg>
             </button>
+            <!--shows the addfolder-->
+            <div id="newfolder" class="hidden">
+                <div class="foldermessage">
+                    <div class="addfoldertab">
+                        <button class="close" onclick="addfolder()">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512">
+                        <path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"/></svg>
+                        </button>
+                        <b class="font">Add new Folder</b>
+                        <!--input folder name-->
+                        <form method="post" enctype="multipart/form-data" action="addfolder.php">
+                            <input type="text" name="foldername" required>
+                            <button>Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            
+            <form id="fileupload" enctype="multipart/form-data" method="post" action="fileupload.php">
 
             <!--if upload button is clicked-->
-            <form method="post" enctype="multipart/form-data" action="fileupload.php">
-                <div class="tap">
+                <input type="text" id="hidden_directory" class="hidden_directory" name="hidden_directory" value="" hidden>
+                <button type="button" class="tap">
                     <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512">
                     <path d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128z"/></svg>
-                    <input type="file" name="file" accept="application/vnd.openxmlformats-officedocument.wordprocessingml.document, application/msword, application/pdf" onchange="form.submit()" class="invisible">
-                </div>
+                    <input type="file" name="file" accept="application/vnd.openxmlformats-officedocument.wordprocessingml.document, application/msword, application/pdf" class="invisible" required>
+                </button>
+                <button type="Submit">text</button>
             </form>
             
             <select placeholder="Sort by">
@@ -146,6 +251,12 @@ else{
                 popcontent.classList.toggle("hidden");
                 popcontent.classList.toggle("visible");
                 }
+                function addfolder() {
+                var popcontent = document.getElementById("newfolder");
+                popcontent.classList.toggle("hidden");
+                popcontent.classList.toggle("visible");
+                }
+                
             </script>
     </body>
 </html>
