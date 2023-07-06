0daca7a322d913e048ba8b43c4e1a8236fde6488
BSONGERISTS <135406685+BSONGERISTS@users.noreply.github.com>
2023-07-06 16:09:03 +0800

    Add files via upload

---
+++ Capstone/OSIA-AboutUs.php
@@ -0,0 +1,55 @@
+
+<html>
+    <head>
+        <link rel="stylesheet" href="OSIA-AboutUsStyle.css">
+        <link rel="icon" type="image/png" href="headerpng.png">
+        <title>OSIA: Organized System Information Assistant</title>
+    </head>
+    <body>
+
+        <div class="Team1">
+            <div class="display1">
+                <img src="Daniboy.svg" height="470px" width="250px" draggable="false">
+            </div>
+        </div>
+        <div class="Team2">
+            <div class="display1">
+                <img src="Daniboy.svg" height="470px" width="250px" draggable="false">
+            </div>
+        </div>
+        <div class="Team3">
+            <div class="display1">
+                <img src="Daniboy.svg" height="470px" width="250px" draggable="false">
+            </div>
+        </div>
+        <div class="Team4">
+            <div class="display1">
+                <img src="Daniboy.svg" height="470px" width="250px" draggable="false">
+            </div>
+        </div>
+        <div class="Team5">
+            <div class="display1">
+                <img src="Daniboy.svg" height="470px" width="250px" draggable="false">
+            </div>
+        </div>
+
+
+        <form action="logout.php">
+            <button class="ToLogin">Go Back</button>
+        </form>
+        
+
+        <div class="element1">
+            <img src="OSIALogo.svg" height="170px" width="170px" draggable="false">
+        </div>
+
+        <div class="email">osia.admin@gmail.com</div>
+        <div class="Bigtext"><b>Our Team</b></div>
+        <div class="Avtext"><b>Meet the people behind the bladiblabalbahlbakljbhakjfhajkfhjkahfjkahfjkahjkfhajkfhakjhfjkahfa</b></div>
+        <div class="Text1"><b>Danica Moster CEO Yarn</b></div>
+        <div class="Text2"><b>Danica Moster CEO Yarn</b></div>
+        <div class="Text3"><b>Danica Moster CEO Yarn</b></div>
+        <div class="Text4"><b>Danica Moster CEO Yarn</b></div>
+        <div class="Text5"><b>Danica Moster CEO Yarn</b></div>
+    </body>
+</html>
